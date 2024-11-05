99643e80c21ef441d4107976553311b6e790e0bf
Mariana Andrade <mariana2003la@gmail.com>
2024-11-05 17:19:29 -0600

    Commit to LoggingExample.java and write to Flat File - Mariana

--- src/Main/LoggingExample.java
+++ src/Main/LoggingExample.java
@@ -13,6 +13,13 @@ public class LoggingExample {
         log.debug("Hello World!");
         log.debug("Hello World, again!");
         log.warn("Hello world!!");
+
+        // Additional logging statements -Mariana
+        System.out.println("Welcome to our game!");
+        log.info("Info: Application is starting.");
+        log.info("Info: Application is running.");
+        log.warn("Warning: This is a warning message.");
+        log.error("Error: An error has occurred when starting the game!");
     }
 
 }
