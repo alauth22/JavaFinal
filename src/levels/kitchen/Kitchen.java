031c5e59183466a538b8652590b77828a8722970
Randy Hawkins <randy.hawkins@bit.nwtc.edu>
2024-11-09 06:54:31 -0600

    RoomBuilder

---
+++ src/levels/kitchen/Kitchen.java
@@ -0,0 +1,25 @@
+package levels.kitchen;
+
+import levels.Room;
+
+public class Kitchen extends Room {
+    @Override
+    public boolean lights(boolean onOff) {
+        return false;
+    }
+
+    @Override
+    public boolean doorsLocked(boolean lockedUnlock) {
+        return false;
+    }
+
+    @Override
+    public boolean windowsLocked(boolean lockedUnlock) {
+        return false;
+    }
+
+    @Override
+    public char getRoomSymbol() {
+        return 'K';
+    }
+}
