904957fff652064268e6000363ed07ad8d0679fb
Mariana Andrade <mariana2003la@gmail.com>
2024-11-15 13:12:34 -0600

    Database and hashmap

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -3,6 +3,7 @@ package Model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class Database {
     private Connection databaseLink;
@@ -23,5 +24,41 @@ public class Database {
         }
 
         return conn;
+    }
+
+    // Method to add an item to the database
+    public void addItem(Item item) throws SQLException {
+        String query = "INSERT INTO items (id, name, quantity) VALUES (?, ?, ?)";
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, item.getId());
+            pstmt.setString(2, item.getName());
+            pstmt.setInt(3, item.getQuantity());
+            pstmt.executeUpdate();
+            System.out.println("Item added: " + item);
+        }
+    }
+
+    // Method to delete an item from the database
+    public void deleteItem(int id) throws SQLException {
+        String query = "DELETE FROM items WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+            System.out.println("Item deleted with ID: " + id);
+        }
+    }
+
+    // Method to update an item's quantity in the database
+    public void updateItemQuantity(int id, int newQuantity) throws SQLException {
+        String query = "UPDATE items SET quantity = ? WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, newQuantity);
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+            System.out.println("Item updated: ID=" + id + ", New Quantity=" + newQuantity);
+        }
     }
 }
